5aa277c697590d8849319d3844b7d203387f04b8
josuel <josuelmroczko>
2023-12-13 15:42:46 -0300

    Adiciona novos recursos

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,11 +1,11 @@
 package application;
 
-import bordGame.Position;
+import bordGame.Board;
+
 
 public class Programa {
     public static void main(String[] args) {
 
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+        Board board= new Board(8,8);
     }
 }
