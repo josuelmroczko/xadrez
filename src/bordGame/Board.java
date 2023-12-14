fa167047d15bb5820a63302689c0cd88b195de51
josuel <josuelmroczko>
2023-12-14 14:53:46 -0300

    if e else das peças

--- src/bordGame/Board.java
+++ src/bordGame/Board.java
@@ -7,37 +7,59 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int colunms) {
+        if (rows < 1 || colunms <1){
+            throw  new BoardException("erro no tabuleiro linhas a menos");
+        }
         this.rows = rows;
         this.colunms = colunms;
         pieces = new Piece[rows][colunms];
     }
 
+
+ ///////////////////////////////////////////////////////////
+
     public int getRows() {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
 
     public int getColunms() {
         return colunms;
     }
-
-    public void setColunms(int colunms) {
-        this.colunms = colunms;
-    }
+///////////////////////////////////////////////////////////
 
     public Piece piece(int row , int colunm){
+        if (!positionExistis(row,colunm)){
+            throw  new BoardException("nao exist e tabiuleiro");
+        }
         return pieces[row][colunm];
     }
     public Piece piece(Position position){
+        if (!positionExistis(position)){
+            throw  new BoardException("nao exist e tabiuleiro");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if (thrreIsApiece(position)){
+            throw new BoardException("ja existe uma peça aqui "+position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
+    private boolean positionExistis (int row,int colunm){
+       return row >= 0  &&  row <= rows &&  colunm >= 0 && colunm < colunms;
+    }
+public boolean positionExistis(Position position){
+   return positionExistis(position.getRow(),position.getColumn());
+}
 
+public boolean thrreIsApiece(Position position){
+    if (!positionExistis(position)){
+        throw  new BoardException("nao exist e tabiuleiro");
+    }
+         return  piece (position) != null;
+
+}
 }
---
+++ src/bordGame/BoardException.java
@@ -0,0 +1,9 @@
+package bordGame;
+
+public class BoardException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+}
